884a034f63bf37736047217f9316f8b57df8048a
teslatickles <hunterhartline@gmail.com>
2018-09-08 16:28:41 -0500

    add homepage with routing

--- src/App.js
+++ src/App.js
@@ -1,58 +1,31 @@
 import React, { Component } from "react";
-import * as firebase from "firebase";
+import { Route, Link } from "react-router-dom";
+import MainChat from "./components/MainChat";
+import Landing from "./components/Landing";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
 import "./App.css";
-import RoomList from "./components/RoomList";
-import MessageList from "./components/MessageList";
-import User from "./components/User";
-
-var config = {
-  apiKey: "AIzaSyAoEMQPlAibTOu-KRI-PkOEQRki7i9n-0Q",
-  authDomain: "bloc-chat-react-c2e5e.firebaseapp.com",
-  databaseURL: "https://bloc-chat-react-c2e5e.firebaseio.com",
-  projectId: "bloc-chat-react-c2e5e",
-  storageBucket: "bloc-chat-react-c2e5e.appspot.com",
-  messagingSenderId: "403233404950"
-};
-firebase.initializeApp(config);
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      activeRoom: "",
-      user: ""
-    };
-  }
-
-  setUser(user) {
-    this.setState({ user: user });
-  }
-
-  handleClick(room) {
-    console.log(room);
-    this.setState({ activeRoom: room });
-  }
-
   render() {
     return (
       <div className="App">
-        <User
-          firebase={firebase}
-          setUser={this.setUser.bind(this)}
-          user={this.state.user}
-        />
-        <RoomList
-          firebase={firebase}
-          handleClick={this.handleClick.bind(this)}
-          activeRoom={this.state.activeRoom}
-        />
-        <MessageList
-          firebase={firebase}
-          handleRoom={this.handleClick.bind(this)}
-          activeRoom={this.state.activeRoom}
-          user={this.state.user}
-        />
+        <header>
+          <nav>
+            <Link className="routes" to="/">
+              Home
+            </Link>
+            <Link className="routes" to="/main-chat">
+              ChatAt
+            </Link>
+          </nav>
+          <h1>ChatAt</h1>
+        </header>
+        <main>
+          <Route exact path="/" component={Landing} />
+          <Route path="/main-chat" component={MainChat} />
+        </main>
       </div>
     );
   }
---
+++ src/components/MainChat.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import * as firebase from "firebase";
+import RoomList from "./RoomList";
+import MessageList from "./MessageList";
+import User from "./User";
+
+var config = {
+  apiKey: "AIzaSyAoEMQPlAibTOu-KRI-PkOEQRki7i9n-0Q",
+  authDomain: "bloc-chat-react-c2e5e.firebaseapp.com",
+  databaseURL: "https://bloc-chat-react-c2e5e.firebaseio.com",
+  projectId: "bloc-chat-react-c2e5e",
+  storageBucket: "bloc-chat-react-c2e5e.appspot.com",
+  messagingSenderId: "403233404950"
+};
+firebase.initializeApp(config);
+
+class MainChat extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      activeRoom: "",
+      user: ""
+    };
+  }
+
+  setUser(user) {
+    this.setState({ user: user });
+  }
+
+  handleClick(room) {
+    console.log(room);
+    this.setState({ activeRoom: room });
+  }
+
+  render() {
+    return (
+      <div className="MainChat">
+        <User
+          firebase={firebase}
+          setUser={this.setUser.bind(this)}
+          user={this.state.user}
+        />
+        <RoomList
+          firebase={firebase}
+          handleClick={this.handleClick.bind(this)}
+          activeRoom={this.state.activeRoom}
+        />
+        <MessageList
+          firebase={firebase}
+          handleRoom={this.handleClick.bind(this)}
+          activeRoom={this.state.activeRoom}
+          user={this.state.user}
+        />
+      </div>
+    );
+  }
+}
+export default MainChat;
